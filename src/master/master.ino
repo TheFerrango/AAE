f42b7b63e9c19ef195f52d85ae6380de82767ba2
TheFerrango <llorenzo@outlook.com>
2013-08-21 11:44:02 +0200

    network improvements
    *added network support to remote project
    *added ringtone support to master and remote
    *changed sensor character from 'D' to 'S' to match sensor labels
    *cleaned up remote project

--- src/master/master.ino
+++ src/master/master.ino
@@ -21,7 +21,7 @@ Timer t;
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);    
 
 // badenia
-int RingTone = 0;
+int RingTone = -1;
 
 // matrici di supporto per lo stato generale
 boolean masterDoorStatus[nDevices][12];
@@ -41,203 +41,224 @@ char udpMsg[UDP_TX_PACKET_MAX_SIZE];
 
 
 /* 
-    decodifica una stringa ricevuta da un pacchetto UDP
-    assegnando i valori alle matrici di supporto
-    contenenti lo stato generale del sistema
-*/
+	decodifica una stringa ricevuta da un pacchetto UDP
+	assegnando i valori alle matrici di supporto
+	contenenti lo stato generale del sistema
+ */
 void parseInputSeq(String s) {
-	
-	int device, pin;
-	boolean newStatus;
-	char type;
-	
-	device = s.substring(1,2).toInt();
-	pin = s.substring(3,5).toInt();
-	newStatus = s.substring(6,7).toInt();
-	type = s[2];
-	
-	if(s[6] == '1')
-		Serial.write("Evento apertura");
-	else
-		Serial.write("Evento chiusura");
-	
-	if(type == 'D')
-		masterDoorStatus[device][pin] = newStatus;
-	else
-		masterAuxStatus[device][pin-34] = newStatus;
-	
+
+  int device, pin;
+  boolean newStatus;
+  char type;
+
+  device = s.substring(1,2).toInt();
+  pin = s.substring(3,5).toInt();
+  newStatus = s.substring(6,7).toInt();
+  type = s[2];
+
+  if(s[6] == '1')
+    Serial.write("Evento apertura");
+  else
+    Serial.write("Evento chiusura");
+
+  if(type == 'S')
+    masterDoorStatus[device][pin] = newStatus;
+  else
+    masterAuxStatus[device][pin] = newStatus;
 }
 
 
 /*
 	aggiorna gli eventi critici mostrati sulla seconda
-	riga del display lcd
-*/
+ 	riga del display lcd
+ */
 void UpdateEvents() {
-	
-	int begDev = devIndexAux;
-	int begSen = senIndexAux;
-	boolean found = false;
-	do
-	{
-		lcd.setCursor(0,1);   
-		if (masterAuxStatus[devIndexAux][senIndexAux])
-		{
-			lcd.print("                ");
-			lcd.setCursor(0,1);   
-			switch (senIndexAux)
-                {
-                case 0:
-                    lcd.print("BATTERIA SCARICA");
-                case 1:
-                    lcd.print("APERTURA SENSORE");
-                case 2:
-                    lcd.print("PERDITA SENSORE");
-                }
-			found = true;			
-		}
-		
-		senIndexAux++;
-		if(senIndexAux == 4)
-		{
-			senIndexAux = 0;
-			devIndexAux = (devIndexAux+1 )% nDevices;
-		}
-		
-		if(found)
-			break;		
-	}
-	while(begDev != devIndexAux || begSen != senIndexAux);
-	
-	if(!found)
-		lcd.print("Nessun evento   ");
+
+  int begDev = devIndexAux;
+  int begSen = senIndexAux;
+  boolean found = false;
+  do
+  {
+    lcd.setCursor(0,1);   
+    if (masterAuxStatus[devIndexAux][senIndexAux])
+    {
+      lcd.print("                ");
+      lcd.setCursor(0,1);   
+      switch (senIndexAux)
+      {
+      case 0:
+        lcd.print("BATTERIA SCARICA");
+      case 1:
+        lcd.print("APERTURA SENSORE");
+      case 2:
+        lcd.print("PERDITA SENSORE");
+      }
+      found = true;			
+    }
+
+    senIndexAux++;
+    if(senIndexAux == 4)
+    {
+      senIndexAux = 0;
+      devIndexAux = (devIndexAux+1 )% nDevices;
+    }
+
+    if(found)
+      break;		
+  }
+  while(begDev != devIndexAux || begSen != senIndexAux);
+
+  if(!found)
+    lcd.print("Nessun evento   ");
 }
 
 
 /*
 	aggiorna gli eventi porta mostrati sulla prima prima
-	riga del display lcd
-*/
+ 	riga del display lcd
+ */
 void UpdateDoors() {
-	
-	int begDev = devIndexDoor;
-	int begSen = senIndexDoor;
-	boolean found = false;
-	do
-	{
-		lcd.setCursor(0,0);   
-		if (masterDoorStatus[devIndexDoor][senIndexDoor])
-		{
-			lcd.print("                ");
-			lcd.setCursor(0,0);   
-			lcd.print("Cen ");
-			lcd.print(devIndexDoor);
-			lcd.print(" - porta ");
-			lcd.print(senIndexDoor);	
-			//digitalWrite(38, HIGH);
-			found = true;			
-		}
-		
-		senIndexDoor++;
-		if(senIndexDoor == senForDev)
-		{
-			senIndexDoor = 0;
-			devIndexDoor = (devIndexDoor+1 )% nDevices;
-		}
-		
-		if(found)
-			break;		
-	}
-	while(begDev != devIndexDoor || begSen != senIndexDoor);
-	
-	if(!found)
-		lcd.print("Nessun evento   ");
+
+  int begDev = devIndexDoor;
+  int begSen = senIndexDoor;
+  boolean found = false;
+  do
+  {
+    lcd.setCursor(0,0);   
+    if (masterDoorStatus[devIndexDoor][senIndexDoor])
+    {
+      lcd.print("                ");
+      lcd.setCursor(0,0);   
+      lcd.print("Cen ");
+      lcd.print(devIndexDoor);
+      lcd.print(" - porta ");
+      lcd.print(senIndexDoor);	
+      //digitalWrite(38, HIGH);
+      found = true;			
+    }
+
+    senIndexDoor++;
+    if(senIndexDoor == senForDev)
+    {
+      senIndexDoor = 0;
+      devIndexDoor = (devIndexDoor+1 )% nDevices;
+    }
+
+    if(found)
+      break;		
+  }
+  while(begDev != devIndexDoor || begSen != senIndexDoor);
+
+  if(!found)
+    lcd.print("Nessun evento   ");
 }
 
 
 /*
 	richiama le funzioni di aggiornamento del
-	display lcd
-*/
+ 	display lcd
+ */
 void ProgressDisplay() {
-	UpdateDoors();
-	UpdateEvents();
+  UpdateDoors();
+  UpdateEvents();
 }
 
 
 /*
 	inizializzazione delle strutture dati e delle
-	componenti hardware utilizzate
-*/
+ 	componenti hardware utilizzate
+ */
 void setup() {
-	
-	// inizializzazione display
-    lcd.begin(16, 2);
 
-	// collocamento cursore in 0,0 e stampa
-    lcd.setCursor(0,0);                       
-    lcd.print("Inizializzazione");
-	
-	// inizializzazione scheda Ethernet
-	Ethernet.begin(mac, ip);
-	Udp.begin(listeningPort);
-	
-	//inizializzazione delle matrici di stati a spento
-    for(int i = 0; i < nDevices; i++)
-	{
-		for(int j = 0; j < 12; j++)
-			masterDoorStatus[i][j] = false;
-		for(int j = 0; j < 4; j++)
-			masterAuxStatus[i][j] = false;
-	}	
-	
-	//inizializzazione indici per display
-	devIndexDoor = senIndexDoor = devIndexAux = senIndexAux = 0;
-	
-	//finta progressbar all'avvio
-	lcd.setCursor(0,1);
-	for(int i = 0; i < 16; i++)
-	{
-		lcd.print("*");
-		delay(300);
-	}
-	
-	//sequenze di input fake usate per test
-	parseInputSeq("C0D04S1");
-	parseInputSeq("C0D11S1");
-	parseInputSeq("C1D06S1");
-	parseInputSeq("C1D05S1");
-	parseInputSeq("C0D03S1");
-	//parseInputSeq("C1A35S1");
-	
-	//avvio del timer per l'aggiornamento del display
-	t.every(5000, ProgressDisplay);
-	
-	lcd.clear();
-	// apro console seriale per vedere lo stato ingresso
-    Serial.begin(9600);                        
+  // inizializzazione display
+  lcd.begin(16, 2);
+
+  // collocamento cursore in 0,0 e stampa
+  lcd.setCursor(0,0);                       
+  lcd.print("Inizializzazione");
+
+  // inizializzazione scheda Ethernet
+  Ethernet.begin(mac, ip);
+  Udp.begin(listeningPort);
+
+  //inizializzazione delle matrici di stati a spento
+  for(int i = 0; i < nDevices; i++)
+  {
+    for(int j = 0; j < 12; j++)
+      masterDoorStatus[i][j] = false;
+    for(int j = 0; j < 4; j++)
+      masterAuxStatus[i][j] = false;
+  }
+
+  //inizializzazione indici per display
+  devIndexDoor = senIndexDoor = devIndexAux = senIndexAux = 0;
+
+  //impostazione del pin di output collegato alla sirena
+  pinMode (38, OUTPUT);
+
+  //finta progressbar all'avvio
+  lcd.setCursor(0,1);
+  for(int i = 0; i < 16; i++)
+  {
+    lcd.print("*");
+    delay(300);
+  }
+
+  //sequenze di input fake usate per test
+  parseInputSeq("C0S04S1");
+  parseInputSeq("C0S11S1");
+  parseInputSeq("C1S06S1");
+  parseInputSeq("C1S05S1");
+  parseInputSeq("C0S03S1");
+  //parseInputSeq("C1A35S1");
+
+  //avvio del timer per l'aggiornamento del display
+  t.every(5000, ProgressDisplay);
+
+  lcd.clear();
+  // apro console seriale per vedere lo stato ingresso
+  Serial.begin(9600);                        
 }
 
 
 void loop() {
-	
-	//richiamo il metodo di aggiornamento dei timer
-	t.update();
-	
-	/*
-		FORMATO: C<#centrale><D -> porta; A -> Aux><#pin>S<0 -> rientro allarme; 1 -> allarme>
-	
-		"C2D04S1" significa quindi:
-		Centrale: 2, Porta: 4, Stato: allarme
-	
-	*/    
-	
-	//ricezione pacchetto udp e aggiornamento matrice se necessario
-	if(Udp.parsePacket())
-	{
-		Serial.write("Ho ricevuto");
-		Udp.read(udpMsg, UDP_TX_PACKET_MAX_SIZE);
-		Serial.write (udpMsg);
-		parseInputSeq(String(udpMsg));
-	}
+
+  //richiamo il metodo di aggiornamento dei timer
+  t.update();
+
+
+  //TODO: leggere stato 1 della chiave
+
+
+  /*
+		FORMATO: C<#centrale><S -> Sensore; A -> Aux><#pin>S<0 -> rientro allarme; 1 -> allarme>
+   	
+   		"C2D04S1" significa quindi:
+   		Centrale: 2, Porta: 4, Stato: allarme
+   	
+   	*/
+
+  //ricezione pacchetto udp e aggiornamento matrice se necessario
+  if(Udp.parsePacket())
+  {
+    Serial.write("Ho ricevuto");
+    Udp.read(udpMsg, UDP_TX_PACKET_MAX_SIZE);
+    Serial.write (udpMsg);
+
+    if(udpMsg[6] == '1')
+    {
+      //se la sirena è già accesa resetta il timer della sirena
+      //fermandolo e ricreandolo
+      if(RingTone != -1)
+        t.stop(RingTone);
+
+      //imposta il pin collegato alla sirena ad 1 Logico per 5 minuti
+      RingTone = t.pulse(38, 5 * 60 * 1000, HIGH);
+    }
+
+    //decodifica del messaggio ricevuto per permettere la
+    //visualizzazione degli eventi
+    parseInputSeq(String(udpMsg));
+  }
 }
+
--- src/remote_centra_01/remote_centra_01.ino
+++ src/remote_centra_01/remote_centra_01.ino
@@ -1,13 +1,18 @@
 /* agosto 2013 gestione porte San Patrignano
-  esempio di programma per gestire display, ingressi e porta eth
-  programma scritto da Lotto Lorenzo e Lotto Alessandro */
+ esempio di programma per gestire display, ingressi e porta eth
+ programma scritto da Lotto Lorenzo e Lotto Alessandro */
 
 #include <LiquidCrystal.h>
 #include <EthernetUdp.h>
 #include <string.h>
-//#include <Timer.h>
+#include <Timer.h>
 #include <Ethernet.h>
 #include <SPI.h>
+
+
+// Definizione delle costanti
+#define senForDev 12
+#define currentDevice 0
 
 // timer manager
 Timer t;
@@ -20,149 +25,209 @@ int DoorValue[12] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 int DoorPin[12] = { 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33 };
 int auxPin[4] = { 34, 35, 36, 37 };
 int auxValue[4] = { 0, 0, 0, 0 };
-int RingTone = 0;
+int RingTone = -1;
 int senIndexDoor = 0;
 int senIndexAux = 0;
-#define senForDev 12
+
 
 // Definizione componenti ethernet
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF };                  // 0xEF
-IPAddress ip(192, 168,1,101);                                         // first remote device .101
+// mac locale: 0xEF   -   first remote device .101
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF };
+IPAddress ip(192, 168,1,101), server(192, 168, 1, 100);
 unsigned int listeningPort = 1701;
 EthernetUDP Udp;
 char udpMsg[UDP_TX_PACKET_MAX_SIZE];
 
 
- 
-void setup()
-{
-   lcd.begin(16, 2);                   // inizializzo il display
-  	lcd.setCursor(1,0);            // finta progressbar all'avvio
-	for(int i = 0; i < 16; i++)
-	{
-	  lcd.print("*");
-	  delay(300);
-	}
 
- lcd.setCursor(0,0);            // cursore in 0 0
- lcd.print("LOTTO GIANNI   ");
- lcd.setCursor(0,1);
- lcd.print("IMP. ELETTRICI ");
- delay (5000);
- 
- for (int i=0; i<12; i++)         // ciclo per la dichiarazione degli ingressi di stato delle porte
+/*	
+	aggiorna gli eventi porta mostrati sulla prima prima
+ 	riga del display lcd  
+*/
+void UpdateDoors() {
+
+  int begSen = senIndexDoor;
+  boolean found = false;
+  do
+  {
+    lcd.setCursor(0,0);   
+    if (DoorValue[senIndexDoor])
+    {
+      lcd.print("                ");
+      lcd.setCursor(0,0);   
+      lcd.print("EVENTO PORTA  ");
+      lcd.print(senIndexDoor);	
+
+      found = true;			
+    }
+
+    senIndexDoor++;
+    if(senIndexDoor == senForDev)
+      senIndexDoor = 0;
+
+    if(found)
+      break;		
+  }
+  while(begSen != senIndexDoor);
+
+  if(!found)
+    lcd.print("Nessun evento   ");
+}
+
+
+void UpdateEvents() {
+
+  int begSen = senIndexAux;
+  boolean found = false;
+  do
+  {
+    lcd.setCursor(0,1);   
+    if (auxValue[senIndexAux])
+    {
+      lcd.print("                ");
+      lcd.setCursor(0,1);   
+      switch (senIndexAux)
+      {
+      case 0:
+        lcd.print("BATTERIA SCARICA");
+      case 1:
+        lcd.print("APERTURA SENSORE");
+      case 2:
+        lcd.print("PERDITA SENSORE");
+      }
+      found = true;			
+    }
+
+    senIndexAux++;
+    if(senIndexAux == 4)
+      senIndexAux = 0;
+    if(found)
+      break;		
+  }
+  while(begSen != senIndexAux);
+
+  if(!found)
+    lcd.print("Nessun evento   ");
+}
+
+
+void ProgressDisplay() {
+  UpdateDoors();
+  UpdateEvents();
+}
+
+
+void SendUdpMessage(int sensor, char type, int senStatus) {
+
+  char msg[6];
+  String s = "C"+ currentDevice + type;
+  if(sensor < 10)
+    s+= "0";
+  s+=String(sensor) + "S" + String(senStatus);
+	
+  s.toCharArray(msg, 6);
+  
+  Udp.beginPacket(server, listeningPort);
+  Udp.write(msg);
+  Udp.endPacket();
+}
+
+
+void setup() {
+
+  // inizializzo il display
+  lcd.begin(16, 2);              
+
+  // finta progressbar all'avvio	
+  lcd.setCursor(1,0);            
+  for(int i = 0; i < 16; i++)
+  {
+    lcd.print("*");
+    delay(300);
+  }
+
+
+  // stampa logo aziendale
+  lcd.setCursor(0,0);            
+  lcd.print("LOTTO GIANNI   ");
+  lcd.setCursor(0,1);
+  lcd.print("IMP. ELETTRICI ");
+  delay (5000);
+
+  // ciclo per la dichiarazione degli ingressi di stato delle porte
+  for (int i=0; i<12; i++)         
     pinMode (DoorPin[i], INPUT);
- for (int i=0; i<4; i++)          // ciclo per la dichiarazione degli ingressi ausiliari
-    pinMode (auxPin[i], INPUT);
- pinMode (38, OUTPUT);
 
-  for(int i = 0; i < 12; i++)     //inizializzazione delle matrici di stati a spento
-    DoorStatus[i] = false;
-  for(int i = 0; i < 4; i++)
-    AuxStatus[i] = false;	
+  // ciclo per la dichiarazione degli ingressi ausiliari
+  for (int i=0; i<4; i++)          
+    pinMode (auxPin[i], INPUT);
+
+  // impostazione del pin di controllo della sirena
+  pinMode (38, OUTPUT);
+
+
+  /*
+  IN TEORIA QUESTO SERVE SOLO SU MASTER
+   
+   //inizializzazione dei vettori di stati a spento
+   for(int i = 0; i < 12; i++)     
+   	DoorStatus[i] = false;
+   for(int i = 0; i < 4; i++)
+   	AuxStatus[i] = false;	
+   	
+   */
+
 
   t.every(2000, ProgressDisplay);
 
-
-  Serial.begin(9600);             // apro console seriale per vedere lo stato ingresso
- 
+  // apro console seriale per vedere lo stato ingresso
+  Serial.begin(9600);
 }
 
-
-/*	aggiorna gli eventi porta mostrati sulla prima prima
-	riga del display lcd  */
-
-void UpdateDoors() {
-
-	int begSen = senIndexDoor;
-	boolean found = false;
-	do
-	{
-		lcd.setCursor(0,0);   
-		if (DoorValue[senIndexDoor])
-		{
-			lcd.print("                ");
-			lcd.setCursor(0,0);   
-			lcd.print("EVENTO PORTA  ");
-			lcd.print(senIndexDoor);	
-			//digitalWrite(38, HIGH);
-			found = true;			
-		}
-
-		senIndexDoor++;
-		if(senIndexDoor == senForDev)
-			senIndexDoor = 0;
-
-		if(found)
-			break;		
-	}
-	while(begSen != senIndexDoor);
-
-	if(!found)
-		lcd.print("Nessun evento   ");
-}
-
-void UpdateEvents()
-{
-	int begSen = senIndexAux;
-	boolean found = false;
-	do
-	{
-		lcd.setCursor(0,1);   
-		if (auxValue[senIndexAux])
-		{
-			lcd.print("                ");
-			lcd.setCursor(0,1);   
-			switch (senIndexAux)
-                {
-                case 0:
-                    lcd.print("BATTERIA SCARICA");
-                case 1:
-                    lcd.print("APERTURA SENSORE");
-                case 2:
-                    lcd.print("PERDITA SENSORE");
-                }
-			found = true;			
-		}
-
-		senIndexAux++;
-		if(senIndexAux == 4)
-			senIndexAux = 0;
-		if(found)
-			break;		
-	}
-	while(begSen != senIndexAux);
-
-	if(!found)
-		lcd.print("Nessun evento   ");
-}
-
-void ProgressDisplay() {
-	UpdateDoors();
-	UpdateEvents();
-}
-
-
-void loop()
-{
+void loop() {
+  
   t.update();
+
   for (int i=0; i<12; i++)
+  {
+    int Tmp = digitalRead(DoorPin[i]);
+    if (Tmp != DoorValue[i])
     {
-      int Tmp = digitalRead(DoorPin[i]);
-      if (Tmp != DoorValue[i])
-        {
-        }
-        
+      if(Tmp == HIGH)
+      {
+        //se la sirena è già accesa resetta il timer della sirena
+        //fermandolo e ricreandolo
+        if(RingTone != -1)
+          t.stop(RingTone);
+
+        //imposta il pin collegato alla sirena ad 1 Logico per 5 minuti
+        RingTone = t.pulse(38, 5 * 60 *1000, HIGH);
+      }
+      SendUdpMessage(i, 'S', Tmp);
+
     }
+
+  }
+
   for (int i=0; i<4; i++)
+  {
+    int Tmp = digitalRead(auxPin[i]);  
+    if (Tmp != auxValue[i])
     {
-      int Tmp = digitalRead(auxPin[i]);  
-      if (Tmp != auxValue[i])
-        {
-        }
+      if(Tmp == HIGH)
+      {
+        //se la sirena è già accesa resetta il timer della sirena
+        //fermandolo e ricreandolo
+        if(RingTone != -1)
+          t.stop(RingTone);
+
+        //imposta il pin collegato alla sirena ad 1 Logico per 5 minuti
+        RingTone = t.pulse(38, 5 * 60 * 1000, HIGH);
+      }
+      SendUdpMessage(i, 'A', Tmp);
     }
-    
-    
-    
- 
+  }
 }
+
+
+
